d7f4e6929d241d97e67c90779c461e24f5198639
Miguel Adán Negrón <manegron@redborder.com>
2020-04-28 18:39:04 +0100

    getting all memcached serverss

--- lib/logstash/filters/radius.rb
+++ lib/logstash/filters/radius.rb
@@ -11,8 +11,6 @@ require_relative "util/location_constant"
 require_relative "util/memcached_config"
 require_relative "store/store_manager"
 
-
-
 class LogStash::Filters::Radius < LogStash::Filters::Base
   include LocationConstant
 
@@ -27,7 +25,7 @@ class LogStash::Filters::Radius < LogStash::Filters::Base
   def register
     @dim_to_druid = [MARKET, MARKET_UUID, ORGANIZATION, ORGANIZATION_UUID, DEPLOYMENT, DEPLOYMENT_UUID, 
                    SENSOR_NAME, SENSOR_UUID, NAMESPACE, SERVICE_PROVIDER, SERVICE_PROVIDER_UUID, NAMESPACE_UUID]
-    @memcached_server = MemcachedConfig::servers.first if @memcached_server.empty?
+    @memcached_server = MemcachedConfig::servers if @memcached_server.empty?
     @memcached = Dalli::Client.new(@memcached_server, {:expires_in => 0, :value_max_bytes => 4000000})
     @store = @memcached.get(RADIUS_STORE) || {}
     @store_manager = StoreManager.new(@memcached)   
